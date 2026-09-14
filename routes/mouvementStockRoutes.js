9f63eebb5215f67395d90f5e3b541bb9ff026c36
NGanna24 <mondesirngannakone@gmail.com>
2026-09-14 14:29:12 +0000

    correction

--- routes/mouvementStockRoutes.js
+++ routes/mouvementStockRoutes.js
@@ -5,7 +5,7 @@ import { authenticateToken, authorize, requireRole } from '../middleware/middlew
 
 const router = express.Router();
 
-router.use(authenticateToken);
+router.use(authenticateToken); 
 
 // Routes spécifiques AVANT /:id
 router.get('/stats', authenticateToken,MouvementStockController.getMouvementsStats);
